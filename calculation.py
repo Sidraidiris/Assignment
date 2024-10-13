379a787ab75131f15490fcfde6f471610ee5f973
Sidraidiris <sidraidriismohamud@gmail.com>
2024-10-13 21:37:45 +0300

    Add files via upload

---
+++ calculation.py
@@ -0,0 +1,13 @@
+#calculation
+num1 = int(input("enter num1: "))
+num2 = int(input("enter num2: "))
+action = str(input("choose action: add(a), sub(s), mult(m), div(d) ->"))
+print("the result is ",end="")
+if action == "a":
+     print(num1 + num2)
+elif action == "s":
+     print(num1 - num2)
+elif action == "m":
+     print(num1 * num2)
+else:
+     print(num1 / num2)
